42f59d956626c5305c7689f684473146f8ed33ce
vladislav.horbatiuk@gmail.com <vladislav.horbatiuk@gmail.com>
2013-01-31 16:26:28 +0200

    Accomodated changes in SimpleHeap interface functions; added tests for new functionality - 'PopHeapElementAtIndex'.

--- SimpleHeap/testSimpleHeap.c
+++ SimpleHeap/testSimpleHeap.c
@@ -17,14 +17,16 @@ int main()
     long input, i;
     double key;
     long *element;
+    tHeapElement *extractedElement;
     char strInp[256];
     InitHeap(&aHeap, 10);
     while (1) {
         printf("1. Add element.\n");
         printf("2. Extract min element.\n");
-        printf("3. Exit.\n");
+        printf("3. Extract element at index.\n");
+        printf("4. Exit.\n");
         scanf("%ld", &input);
-        if (input == 3) {
+        if (input == 4) {
             break;
         }
         if (1 == input)
@@ -36,7 +38,7 @@ int main()
             element = malloc(sizeof(long));
             if (!element) printf("element error\n");
             *element = input;
-            if (!AddElementToHeap(&aHeap, key, element))
+            if (!AddElementToHeap(&aHeap, key, element, NULL))
                 printf("Added new element successfully\n");
             else { printf("Failed to add element, probably memory problems...\n"); break; }
                 
@@ -44,18 +46,37 @@ int main()
         else if (2 == input)
         {
             printf("Extracting min element...\n");
-            tHeapElement *minElement = ExtractMinElement(&aHeap);
-            if (minElement)
-                printf("Element key:%lf, element itself:%ld.\n", minElement->key, *(long*) minElement->element);
+            extractedElement = ExtractMinElement(&aHeap, NULL);
+            if (extractedElement)
+            {
+                printf("Element key:%lf, element itself:%ld.\n", extractedElement->key, *(long*) extractedElement->element);
+                free(extractedElement->element);
+                free(extractedElement);
+            }
             else
                 printf("No elements in heap.\n");
         }
+        else if (3 == input)
+        {
+            printf("Enter element index:\n");
+            scanf("%ld", &input);
+            extractedElement = PopHeapElementAtIndex(&aHeap, input, NULL);
+            if (!extractedElement)
+                printf("Index out of bounds.\n");
+            else
+            {
+                printf("Element key:%lf, element itself:%ld\n", extractedElement->key, *(long*) extractedElement->element);
+                free(extractedElement->element);
+                free(extractedElement);                
+            }
+        }
+
         printf("Current heap keys:\n");
         elements = aHeap.elements;
         for (i = 0; i < elements.currentNum; ++i)
         	printf("%lf ", ((tHeapElement*)GetElementAt(&elements, i))->key);
         printf("\n");
     }
-    DisposeHeap(&aHeap);
+    DisposeHeapWithElements(&aHeap);
     return 0;
 }
